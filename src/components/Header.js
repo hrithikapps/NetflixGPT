d954f5466459517d22f77af5a1b99396450e16eb
hrithikapps <hrithikk2765@gmail.com>
2024-04-03 02:22:09 +0530

    updated header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,10 +9,17 @@ import { toggleGPTsearch } from "../utils/GPTslice";
 import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
-
-  const dispatch = useDispatch();
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {})
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
 
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (user) => {
@@ -33,70 +40,53 @@ const Header = () => {
       }
     });
 
+    // UnSubscribe when component unmounts
     return () => unSubscribe();
   }, []);
 
   const handleGPTSearchClick = () => {
+    // Toggle GPT Search
     dispatch(toggleGPTsearch());
   };
-
-  const GPTsearchVisible = useSelector(
-    (store) => store.gpt.ToggleGPTsearchView
-  );
 
   const handleLanguageChange = (e) => {
     dispatch(changeLanguage(e.target.value));
   };
 
-  const handleSignOut = () => {
-    signOut(auth)
-      .then(() => {
-        navigate("/");
-      })
-      .catch((error) => {
-        navigate("/Error");
-      });
-  };
   return (
-    <div className="absolute scrollbar-hide w-[99%] overflow-clip px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
-      <img
-        className="scrollbar-hide w-44 mx-auto md:mx-0"
-        src={LOGO}
-        alt="Logo"
-      />
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
+      <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
       {user && (
-        <div className="flex p-2 justify-between gap-2 items-center">
-          {GPTsearchVisible && (
+        <div className="flex p-2 gap-2 justify-between">
+          {showGptSearch && (
             <select
+              className="p-2 m-2 bg-gray-900 text-white"
               onChange={handleLanguageChange}
-              className="py-1 px-7 m-2 bg-gray-500 text-white"
             >
-              {SUPPORTED_LANGUAGES.map((lang) => {
-                return (
-                  <option key={lang.identifier} value={lang.identifier}>
-                    {lang.name}
-                  </option>
-                );
-              })}
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
             </select>
           )}
-          <div onClick={handleGPTSearchClick}>
-            <button className="rounded-md px-3 py-2 bg-green-700">
-              {GPTsearchVisible ? "Homepage" : "GPT Search"}
-            </button>
-          </div>
-          <img
-            className="hidden md:block w-12 h-12"
-            src={USER_AVATAR}
-            alt="Avatar"
-          />
-          <button onClick={handleSignOut} className="text-white font-bold">
-            Sign Out
+          <button
+            className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
+            onClick={handleGPTSearchClick}
+          >
+            {showGptSearch ? "Homepage" : "GPT Search"}
           </button>
+          <div onClick={handleSignOut} className="flex items-center">
+            <img
+              className="hidden md:block w-10 h-10 rounded-lg"
+              src={USER_AVATAR}
+              alt="userIcon"
+            />
+            <button className=" text-white ">Sign Out</button>
+          </div>
         </div>
       )}
     </div>
   );
 };
-
 export default Header;
